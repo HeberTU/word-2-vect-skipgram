5f67322e2debbf013247fe16d3644532f948f771
Trujillo <HTRUJILLO@lucta.com>
2020-08-07 16:48:03 +0200

    Implementing Process function to pre-processs the text file

--- utilities/data_prep.py
+++ utilities/data_prep.py
@@ -4,8 +4,24 @@ Created on Fri Aug  7 16:13:10 2020
 
 @author: HTRUJILLO
 """
+import re
+from collections import Counter
 
 def load_data(file):
+    '''
+    Read a text document containing the corpus which is the embedding iinput
+
+    Parameters
+    ----------
+    file : .txt o plain text file
+        The file containing the corpus
+
+    Returns
+    -------
+    text : str
+        A long string containing the corpus 
+
+    '''
     
     with open(file) as f:
         text = f.read()
@@ -13,3 +29,43 @@ def load_data(file):
     print("-----file example-----")
     print(text[:100])
     return text
+
+
+def preprocess(text):
+    '''
+    A function that tokenize the corpus creating especial tokens for punctuation signs.
+    Returns only words appearing more than 5 times
+
+    Parameters
+    ----------
+    text : str
+        A long string containing the corpus 
+
+    Returns
+    -------
+    trimmed_words : list
+        List containing words in the document as elements.
+
+    '''
+    
+    # Replace punctuation with tokens so we can use them in our model
+    text = text.lower()
+    text = text.replace('.', ' <PERIOD> ')
+    text = text.replace(',', ' <COMMA> ')
+    text = text.replace('"', ' <QUOTATION_MARK> ')
+    text = text.replace(';', ' <SEMICOLON> ')
+    text = text.replace('!', ' <EXCLAMATION_MARK> ')
+    text = text.replace('?', ' <QUESTION_MARK> ')
+    text = text.replace('(', ' <LEFT_PAREN> ')
+    text = text.replace(')', ' <RIGHT_PAREN> ')
+    text = text.replace('--', ' <HYPHENS> ')
+    text = text.replace('?', ' <QUESTION_MARK> ')
+    # text = text.replace('\n', ' <NEW_LINE> ')
+    text = text.replace(':', ' <COLON> ')
+    words = text.split()
+    
+    # Remove all words with  5 or fewer occurences
+    word_counts = Counter(words)
+    trimmed_words = [word for word in words if word_counts[word] > 5]
+    print(trimmed_words[:30])
+    return trimmed_words
--- word2vec_main.py
+++ word2vec_main.py
@@ -7,7 +7,9 @@ Created on Fri Aug  7 16:08:50 2020
 import os 
 os.chdir("C:/Users/htrujillo/projects/Implementing_Word2Vec_SkipGram")
 
-from utilities.data_prep import load_data
+from utilities.data_prep import load_data, preprocess
 
 
 text = load_data("data/text8")
+
+words = preprocess(text)
