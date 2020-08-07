f6a7a69b9918b9a23129f1e609e6a6f963027d96
Heber <heber.trj.urt@gmail.com>
2020-08-07 16:11:29 +0200

    utilities dir added

---
+++ utilities/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  7 16:10:36 2020
+
+@author: HTRUJILLO
+"""
---
+++ word2vec_main.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  7 16:08:50 2020
+
+@author: HTRUJILLO
+"""
