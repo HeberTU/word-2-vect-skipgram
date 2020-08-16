928f30c5bf173fcc74ee35d2569379127071a731
Trujillo <heber.trj.urt@gmail.com>
2020-08-16 16:38:05 +0200

    tsne vis functionality added

--- visualization/tsne_vis.py
+++ visualization/tsne_vis.py
@@ -8,3 +8,13 @@ import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 
 
+def tsne_vis(model, int_to_vocab, vis_words = 100):
+
+    embeddings = model.embed.weight.to('cpu').data.numpy()
+    tsne = TSNE()
+    embed_tsne = tsne.fit_transform(embeddings[:vis_words, :])
+    
+    fig, ax = plt.subplots(figsize=(16,16))
+    for idx in range(vis_words):
+        plt.scatter(*embed_tsne[idx, :], color='steelblue')
+        plt.annotate(int_to_vocab[idx], (embed_tsne[idx, 0], embed_tsne[idx, 1]), alpha=0.7)
