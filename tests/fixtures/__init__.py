351fad8fbd5daedd527073bfb534d37ad789af73
HeberTU <heber.trujillo@glovoapp.com>
2022-07-21 12:16:30 +0200

    feat: fixture test library created.

---
+++ tests/fixtures/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Testing fixtures library.
+
+Created on: 21/7/22
+@author: Heber Trujillo <heber.trj.urt@gmail.com>
+Licence,
+"""
+from .loss_functions import (
+    LossArtifactsType,
+    get_loss_artifacts,
+)
+
+__all__ = ["get_loss_artifacts", "LossArtifactsType"]
