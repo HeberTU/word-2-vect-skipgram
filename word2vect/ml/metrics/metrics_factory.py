8d5b3324f54f5e34bb7b7eb837e8031e76474a89
HeberTU <heber.trujillo@glovoapp.com>
2022-07-27 08:39:05 +0200

    feat: metrics data structure configurations created.

---
+++ word2vect/ml/metrics/metrics_factory.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""Metrics factory module.
+
+The module provides the constructor factory for the different metrics
+implementations, as well as the configuration structures.
+
+
+Created on: 27/7/22
+@author: Heber Trujillo <heber.trj.urt@gmail.com>
+Licence,
+"""
+from __future__ import annotations
+
+import enum
+from dataclasses import dataclass
+from typing import (
+    Any,
+    Dict,
+    Optional,
+)
+
+
+@dataclass(frozen=True)
+class ModelMetrics:
+    """Data structure to store model's metrics set-up."""
+
+    optimizing_metric: str
+
+
+@dataclass(frozen=True)
+class MetricConfig:
+    """Data structure for storing metric configuration."""
+
+    metric_type: MetricType
+    params: Optional[Dict[str, Any]] = None
+
+
+class MetricType(enum.Enum):
+    """Available metrics."""
+
+    F1 = enum.auto()
+    PRECISION = enum.auto()
+    RECALL = enum.auto()
+
+
+class AverageStrategy(enum.Enum):
+    """Available metrics reduction strategies."""
+
+    MICRO = "micro"
+    MACRO = "macro"
+    WEIGHTED = "weighted"
