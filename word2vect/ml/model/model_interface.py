370a20d9f20bbcdcd91a01357438fd6ec69f3ceb
HeberTU <heber.trujillo@glovoapp.com>
2022-08-01 09:25:07 +0200

    feat: model library created.

---
+++ word2vect/ml/model/model_interface.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Interface model module.
+
+This module implement the abstract model class, which is used to define
+the common interface across the different model implementations.
+
+Created on: 1/8/22
+@author: Heber Trujillo <heber.trj.urt@gmail.com>
+Licence,
+"""
