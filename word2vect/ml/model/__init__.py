cf59286bba07aa4dc5f94a1ef8e0a17aba124cbe
HeberTU <heber.trujillo@glovoapp.com>
2022-08-03 08:43:43 +0200

    fix: adding missing imports to model library.

--- word2vect/ml/model/__init__.py
+++ word2vect/ml/model/__init__.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
-"""
+"""Model Library.
 
 Created on: 23/6/22
 @author: Heber Trujillo <heber.trj.urt@gmail.com>
 Licence,
 """
+from .model_factory import ModelType
+from .model_interface import (
+    BatchData,
+    ModelConfig,
+)
+from .word2vect_model import Word2VectModel
+
+__all__ = [
+    "Word2VectModel",
+    "BatchData",
+    "ModelConfig",
+    "ModelType",
+]
