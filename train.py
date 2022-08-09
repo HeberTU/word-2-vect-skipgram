09a3a64d372d58441ba33d85dd9455b86a2770ce
HeberTU <heber.trujillo@glovoapp.com>
2022-08-09 08:28:43 +0200

    ref: introducing model factory at run time.

--- train.py
+++ train.py
@@ -6,7 +6,6 @@ Created on: 21/7/22
 Licence,
 """
 from torch import nn
-from torch.optim import SGD
 
 from word2vect.data.data_prep import (
     create_lookup_tables,
@@ -16,7 +15,7 @@ from word2vect.data.data_prep import (
 from word2vect.ml import (
     loss_functions,
     metrics,
-    model,
+    models,
     networks,
 )
 
@@ -27,63 +26,48 @@ def main():
     words = preprocess(text)
     vocab_to_int, int_to_vocab = create_lookup_tables(words)
 
-    network_config = networks.NetworkConfig(
-        features=networks.Features(
-            vocabulary=networks.Vocabulary(
-                size=len(vocab_to_int),
-                vocabulary_to_idx=vocab_to_int,
-                idx_to_vocabulary=int_to_vocab,
-            ),
-            embedding_dim=300,
+    model_definition = models.ModelDefinition(
+        model_config=models.ModelConfig(
+            model_type=models.ModelType.WORD2VECT,
+            model_name="word2vect_v01",
+            gradient_clipping_value=1,
         ),
-        hidden_layers=networks.HiddenLayers(
-            hidden_dim=None, activation=None, dropout=None
-        ),
-        output_layer=networks.OutputLayer(activation=nn.LogSoftmax(dim=1)),
-    )
-
-    network = networks.NetworkFactory(network_config=network_config).create(
-        network_architecture=networks.NetworkArchitecture.SKIPGRAM
-    )
-    metrics_config = metrics.MetricsConfig(
-        optimizing_metric=metrics.MetricConfig(
-            metric_type=metrics.MetricType.F1, params={"average": "macro"}
-        ),
-        secondary_metrics=[
-            metrics.MetricConfig(
-                metric_type=metrics.MetricType.PRECISION,
-                params={"average": "macro"},
+        network_config=networks.NetworkConfig(
+            features=networks.Features(
+                vocabulary=networks.Vocabulary(
+                    size=len(vocab_to_int),
+                    vocabulary_to_idx=vocab_to_int,
+                    idx_to_vocabulary=int_to_vocab,
+                ),
+                embedding_dim=300,
             ),
-            metrics.MetricConfig(
-                metric_type=metrics.MetricType.RECALL,
-                params={"average": "macro"},
+            hidden_layers=networks.HiddenLayers(
+                hidden_dim=None, activation=None, dropout=None
             ),
-        ],
+            output_layer=networks.OutputLayer(activation=nn.LogSoftmax(dim=1)),
+        ),
+        metrics_config=metrics.MetricsConfig(
+            optimizing_metric=metrics.MetricConfig(
+                metric_type=metrics.MetricType.F1, params={"average": "macro"}
+            ),
+            secondary_metrics=[
+                metrics.MetricConfig(
+                    metric_type=metrics.MetricType.PRECISION,
+                    params={"average": "macro"},
+                ),
+                metrics.MetricConfig(
+                    metric_type=metrics.MetricType.RECALL,
+                    params={"average": "macro"},
+                ),
+            ],
+        ),
+        loss_function_config=loss_functions.LossFunctionConfig(
+            loss_function_type=loss_functions.LossFunctionType.NLLLOSS
+        ),
     )
 
-    model_metrics = metrics.ModelMetricsFactory(
-        metrics_config=metrics_config
-    ).create()
-
-    model_config = model.ModelConfig(
-        model_type=model.ModelType.WORD2VECT,
-        model_name="word2vect_v01",
-        gradient_clipping_value=1,
-    )
-
-    optimizer = SGD(network.parameters(), lr=0.1, momentum=0.9)
-
-    loss_function = loss_functions.LossFunctionFactory(
-        loss_function_config=loss_functions.LossFunctionConfig()
-    ).create(loss_function_type=loss_functions.LossFunctionType.NLLLOSS)
-
-    word2vect_model = model.Word2VectModel(
-        network=network,
-        model_metrics=model_metrics,
-        model_config=model_config,
-        optimizer=optimizer,
-        loss_function=loss_function,
-    )
+    model_factory = models.ModelFactory(model_definition=model_definition)
+    word2vect_model = model_factory.create()
 
     return word2vect_model
 
