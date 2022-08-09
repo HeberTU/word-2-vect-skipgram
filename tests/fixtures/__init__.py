fc7e1da03062d765e9f25b2f141cabb6360bbeba
HeberTU <heber.trujillo@glovoapp.com>
2022-08-09 09:46:47 +0200

    feat: new implementations added at fixture module level.

--- tests/fixtures/__init__.py
+++ tests/fixtures/__init__.py
@@ -5,16 +5,32 @@ Created on: 21/7/22
 @author: Heber Trujillo <heber.trj.urt@gmail.com>
 Licence,
 """
-from .loss_functions import get_loss_artifacts
-from .metrics import get_metrics_artifacts
+from .loss_functions import (
+    get_loss_artifacts,
+    get_loss_function_config,
+)
+from .metrics import (
+    get_metrics_artifacts,
+    get_metrics_config,
+)
+from .models import (
+    get_model_artifacts,
+    get_model_config,
+)
 from .network import (
     get_network_artifacts,
     get_network_config,
 )
+from .optimizers import get_optimizer_config
 
 __all__ = [
     "get_loss_artifacts",
     "get_network_artifacts",
     "get_metrics_artifacts",
     "get_network_config",
+    "get_model_artifacts",
+    "get_model_config",
+    "get_loss_function_config",
+    "get_optimizer_config",
+    "get_metrics_config",
 ]
