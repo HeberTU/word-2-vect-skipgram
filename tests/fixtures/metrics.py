20228dc695496d7dd69cfea6514fb7ee3912a05e
HeberTU <heber.trujillo@glovoapp.com>
2022-08-09 09:43:26 +0200

    feat: metrics configuration fixture created.

--- tests/fixtures/metrics.py
+++ tests/fixtures/metrics.py
@@ -11,7 +11,10 @@ from typing import (
     Union,
 )
 
-from word2vect.ml import metrics
+from word2vect.ml import (
+    metrics,
+    models,
+)
 
 
 def get_metrics_artifacts(
@@ -54,3 +57,37 @@ def get_metrics_artifacts(
     metric_artifacts = _implementations.get(metric_type, None)
 
     return metric_artifacts
+
+
+def get_metrics_config(model_type: models.ModelType) -> metrics.MetricsConfig:
+    """Get metrics configuration for model.
+
+    This function temporarily only accepts the Word2Vect config.
+
+    Returns:
+        metrics_config: metrics configuration.
+    """
+    params = {"average": "macro"}
+
+    _implementation = {
+        models.ModelType.WORD2VECT: {
+            "optimizing_metric": metrics.MetricType.F1,
+            "secondary_metrics": [
+                metrics.MetricType.PRECISION,
+                metrics.MetricType.RECALL,
+            ],
+        },
+    }
+
+    _metrics = _implementation.get(model_type, None)
+
+    return metrics.MetricsConfig(
+        optimizing_metric=metrics.MetricConfig(
+            metric_type=_metrics.get("optimizing_metric"),
+            params=params,
+        ),
+        secondary_metrics=[
+            metrics.MetricConfig(metric_type=metric_type, params=params)
+            for metric_type in _metrics.get("secondary_metrics")
+        ],
+    )
