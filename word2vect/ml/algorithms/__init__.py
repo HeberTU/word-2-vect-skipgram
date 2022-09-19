db7873f3613e2fde2be25c2c7ee79871d1b536fa
HeberTU <heber.trujillo@glovoapp.com>
2022-09-19 11:57:46 +0200

    feat: algorithm library init.

--- word2vect/ml/algorithms/__init__.py
+++ word2vect/ml/algorithms/__init__.py
@@ -5,3 +5,18 @@ Created on: 17/8/22
 @author: Heber Trujillo <heber.trj.urt@gmail.com>
 Licence,
 """
+from .algorithm import Algorithm
+from .algorithm_factory import (
+    AlgorithmConfig,
+    AlgorithmFactory,
+    AlgorithmType,
+)
+from .network_algorithm import NetworkAlgorithm
+
+__all__ = [
+    "Algorithm",
+    "AlgorithmConfig",
+    "AlgorithmType",
+    "AlgorithmFactory",
+    "NetworkAlgorithm",
+]
