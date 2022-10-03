c57a14c452af00679023687be237ed4b6e97cb22
HeberTU <heber.trujillo@glovoapp.com>
2022-10-03 08:01:11 +0200

    fix: skipgram collate function created.

--- word2vect/data/loaders/__init__.py
+++ word2vect/data/loaders/__init__.py
@@ -5,8 +5,7 @@ Created on: 23/6/22
 @author: Heber Trujillo <heber.trj.urt@gmail.com>
 Licence,
 """
+from .collate_functions import skipgram_collate
 from .dataset import W2VDataset
 
-__all__ = [
-    "W2VDataset",
-]
+__all__ = ["W2VDataset", "skipgram_collate"]
