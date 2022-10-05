a4dccf9ae09189f3766b2ef8d92271e0a8ff262e
HeberTU <heber.trujillo@glovoapp.com>
2022-10-05 07:57:15 +0200

    feat: adding sample probabilities to get data.

--- tests/fixtures/data_loaders.py
+++ tests/fixtures/data_loaders.py
@@ -8,6 +8,7 @@ Licence,
 import random
 import string
 
+import numpy as np
 from torch.utils.data import DataLoader
 
 from word2vect.data import loaders
@@ -36,37 +37,48 @@ def get_vocabulary_space(vocabulary_size: int = 20) -> networks.Vocabulary:
     )
 
 
-def get_dataset(dataset_size: int = 20) -> loaders.W2VDataset:
+def get_dataset(
+    dataset_size: int = 20, batch_size: int = 5
+) -> loaders.W2VDataset:
     """Get torch data loader.
 
     Args:
         dataset_size: Number of words in the dataset.
-
+        batch_size: number of samples per batch.
     Returns:
         w2vdataset: Word-2-Vect dataset.
     """
     vocabulary = get_vocabulary_space()
 
+    probs = [2**x for x in range(vocabulary.size)]
+    probs = [x / sum(probs) for x in probs]
+
     random.seed(27)
 
-    words = random.choices(
-        population=list(vocabulary.idx_to_vocabulary.keys()), k=dataset_size
+    words = np.random.choice(
+        a=list(vocabulary.idx_to_vocabulary.keys()),
+        size=dataset_size,
+        replace=True,
+        p=probs,
     )
 
-    return loaders.W2VDataset(words=words)
+    return loaders.W2VDataset(
+        words=words, batch_size=batch_size, threshold=1 / vocabulary.size
+    )
 
 
-def get_data_loader(batch_size: int) -> DataLoader:
+def get_data_loader(dataset_size: int = 20, batch_size: int = 5) -> DataLoader:
     """Get pytorch data loader.
 
     Args:
+        dataset_size: Number of words in the dataset.
         batch_size: number of samples per batch.
 
     Returns:
         data_loader: An instance of pytorch's data loader.
     """
     return DataLoader(
-        dataset=get_dataset(),
+        dataset=get_dataset(dataset_size, batch_size),
         batch_size=batch_size,
         shuffle=False,
         collate_fn=loaders.skipgram_collate,
